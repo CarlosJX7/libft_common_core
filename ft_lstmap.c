bd4bb682af8c90b91dc93dcd1d2b0584c78ced33
CarlosJX7 <carlos.jx7@gmail.com>
2026-05-28 14:21:38 +0200

    fin

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,34 +12,32 @@
 
 #include "libft.h"
 
-static t_list	*ft_lstmap_error(t_list **first_node, void (*del)(void *),
-		void *new_content)
+static t_list	*ft_lstmap_error(t_list **lst, void (*del)(void *),
+		void *content)
 {
-	if (new_content)
-		del(new_content);
-	ft_lstclear(first_node, del);
+	if (content)
+		del(content);
+	ft_lstclear(lst, del);
 	return (NULL);
 }
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	void	*new_content;
-	t_list	*first_node;
+	t_list	*new_lst;
 	t_list	*new_node;
+	void	*new_content;
 
-	first_node = NULL;
 	if (!f || !del)
 		return (NULL);
+	new_lst = NULL;
 	while (lst)
 	{
 		new_content = f(lst->content);
-		if (!new_content)
-			return (ft_lstmap_error(&first_node, del, NULL));
 		new_node = ft_lstnew(new_content);
 		if (!new_node)
-			return (ft_lstmap_error(&first_node, del, new_content));
-		ft_lstadd_back(&first_node, new_node);
+			return (ft_lstmap_error(&new_lst, del, new_content));
+		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
-	return (first_node);
+	return (new_lst);
 }
